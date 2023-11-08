89021d80b86dcdeb5a3ac3b1e613836beb9f8e16
Rizki Ramadhan <farazrizki13@gmail.com>
2023-11-09 01:30:10 +0700

    campaign canvas

--- js/main.js
+++ js/main.js
@@ -467,56 +467,7 @@ $(function () {
     } // init donut chart if element exists
 
 
-    if ($("#barChart1").length) {
-      var barChart1 = $("#barChart1");
-      var barData1 = {
-        labels: ["January", "February", "March", "April", "May", "June"],
-        datasets: [{
-          label: "My First dataset",
-          backgroundColor: ["#5797FC", "#629FFF", "#6BA4FE", "#74AAFF", "#7AAEFF", '#85B4FF'],
-          borderColor: ['rgba(255,99,132,0)', 'rgba(54, 162, 235, 0)', 'rgba(255, 206, 86, 0)', 'rgba(75, 192, 192, 0)', 'rgba(153, 102, 255, 0)', 'rgba(255, 159, 64, 0)'],
-          borderWidth: 1,
-          data: [24, 42, 18, 34, 56, 28]
-        }]
-      }; // -----------------
-      // init bar chart
-      // -----------------
-
-      new Chart(barChart1, {
-        type: 'bar',
-        data: barData1,
-        options: {
-          scales: {
-            xAxes: [{
-              display: false,
-              ticks: {
-                fontSize: '11',
-                fontColor: '#969da5'
-              },
-              gridLines: {
-                color: 'rgba(0,0,0,0.05)',
-                zeroLineColor: 'rgba(0,0,0,0.05)'
-              }
-            }],
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              },
-              gridLines: {
-                color: 'rgba(0,0,0,0.05)',
-                zeroLineColor: '#6896f9'
-              }
-            }]
-          },
-          legend: {
-            display: false
-          },
-          animation: {
-            animateScale: true
-          }
-        }
-      });
-    } // init pie chart if element exists
+     // init pie chart if element exists
 
 
     if ($("#pieChart1").length) {
